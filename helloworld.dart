f43248174ba4e9a1bbdf41191a4d553fac46858a
shinmj <gkdlqkek@gmail.com>
2023-06-21 22:48:05 +0900

    refactor:
    - 설명 수정.
    - 파일명 수정.

--- helloworld.dart
+++ helloworld.dart
@@ -1,4 +1,12 @@
 // 모든 코드는 main 함수 안에 있어야 함.
+// main 함수만 실행 가능.
 void main() {
-  print('hello world'); //세미콜론은 반드시 넣어주어야 함.
+  print('hello world'); //세미콜론은 반드시 넣어주어야 함.\
+
+  hello();
+}
+
+// main 함수 안에서 호출해주어야 실행 가능.
+void hello() {
+  print('hello');
 }
